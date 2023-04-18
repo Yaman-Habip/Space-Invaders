a7c399137a1d17321d66b49fba6032d67c1ace6d
aktejo <abhiram_tejomurtula25@milton.edu>
2023-04-18 09:54:46 -0400

    new class

---
+++ src/Boba.java
@@ -0,0 +1,40 @@
+import java.awt.Rectangle;
+
+public class Boba {
+    public int xpos;
+    public double ypos;
+    public Rectangle rect;
+    public boolean most_recent = true;
+
+    public Boba(int x, double y){
+        xpos = x;
+        ypos = y;
+    }
+
+    public void build_rect(){
+        int y = (int) Math.round(ypos);
+        rect = new Rectangle(xpos, y, 75,75);
+    }
+
+    public boolean move(Boba b, Boba b1, double i){
+        ypos += i;
+        if (! (b.xpos >= 800 && b1.xpos <= 50)) {
+            if (b.xpos >= 800) {
+                xpos -= 1.5;
+                most_recent = false;
+            } else if (!most_recent) {
+                xpos -= 1.5;
+            } else {
+                xpos += 1.5;
+            }
+            if (b1.xpos <= 50) {
+                most_recent = true;
+            }
+        }
+        build_rect();
+        if (ypos >= 550) {
+            return false;
+        }
+        return true;
+    }
+}
