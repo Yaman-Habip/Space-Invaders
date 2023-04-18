82f3558c413ac8287cae9a11112936e75f631e3e
Yaman Habip <56274952+Yaman-Habip@users.noreply.github.com>
2023-04-18 08:59:50 -0400

    added line break

--- src/Setup.java
+++ src/Setup.java
@@ -15,6 +15,7 @@ public class Setup implements Runnable, KeyListener {
     final int HEIGHT = 700;
 
     public int score = 0;
+
     public int high_score = 0;
     public boolean new_high_score = false;
 
